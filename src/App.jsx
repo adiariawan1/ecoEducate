78f5070acb7dfa3a16ba4c7b959c73d8b3ed9a15
adi ariawan <adiariawan1140@gmail.com>
2026-01-10 21:42:50 +0800

    update navbar dan data data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+// src/App.jsx
 import React from 'react'
+import { Routes, Route } from 'react-router-dom' // HAPUS 'Router' atau 'BrowserRouter'
 import DefaultLayouts from './components/layouts/defaultLayouts'
 import Jumbotron from './components/home/jumbotron'
+
 const App = () => {
   return (
     <DefaultLayouts>
-      <Jumbotron/>
+      <Routes>
+          <Route path="/" element={<Jumbotron />} />
+          <Route path="/donate" element={<div className="pt-24 text-center">Halaman Donasi</div>} />
+      </Routes>
     </DefaultLayouts>
   )
 }
--- src/components/home/jumbotron/index.jsx
+++ src/components/home/jumbotron/index.jsx
@@ -3,9 +3,9 @@ import Navbar from '../navbar'
 
 const Jumbotron = () => {
   return (
-    <div>
-        <Navbar/>
-        <h1>ini Jumbotron</h1>
+    <div className='relative'>
+      <Navbar/>
+
     </div>
   )
 }
